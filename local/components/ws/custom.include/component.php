640832b57ae2145a6c732cf7f27cd8ed3f9c9c82
afelia_master <p.raskolnikov@wssupport.ru>
2021-06-25 09:14:06 +0300

    kp65131 правки структуры

--- local/components/ws/custom.include/component.php
+++ local/components/ws/custom.include/component.php
@@ -1,10 +1,6 @@
-<?
+<?php
 /**
- * @global CMain $APPLICATION
- * @global CUser $USER
- * @global CUserTypeManager $USER_FIELD_MANAGER
  * @var array $arParams
- * @var CBitrixComponent $this
  */
 
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
